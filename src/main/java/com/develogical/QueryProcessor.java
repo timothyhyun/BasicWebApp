7afc87cec08aec727416b7553343013dd6436de5
Timothy Hyun <Timothy>
2022-09-22 15:55:48 -0400

    banana

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,6 +7,9 @@ public class QueryProcessor {
             String[] words = query.split(" ");
             return String.valueOf(Integer.parseInt(words[words.length-1]) + Integer.parseInt(words[words.length-3]));
         }
+        if (query.toLowerCase().contains("banana")) {
+            return "yellow";
+        }
         return "as;lfkjas;lk";
 
     }
