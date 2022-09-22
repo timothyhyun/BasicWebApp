a29f1077a872218ff56faff172a7ed2830567b1e
Timothy Hyun <Timothy>
2022-09-22 15:53:03 -0400

    adsf

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,6 +7,7 @@ public class QueryProcessor {
             String[] words = query.split(" ");
             return String.valueOf(Integer.parseInt(words[words.length-1]) + Integer.parseInt(words[words.length-3]));
         }
-        return "";
+        return "as;lfkjas;lk";
+
     }
 }
