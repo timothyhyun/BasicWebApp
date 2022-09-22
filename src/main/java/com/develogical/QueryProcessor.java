0e320536c5aa87a46428381140e3c79e7d215949
Timothy Hyun <Timothy>
2022-09-22 15:59:13 -0400

    bond

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,8 +7,16 @@ public class QueryProcessor {
             String[] words = query.split(" ");
             return String.valueOf(Integer.parseInt(words[words.length-1]) + Integer.parseInt(words[words.length-3]));
         }
+        if (query.toLowerCase().contains("multiplied")) {
+            String[] words = query.split(" ");
+            return String.valueOf(Integer.parseInt(words[words.length-1]) * Integer.parseInt(words[words.length-3]));
+        }
         if (query.toLowerCase().contains("banana")) {
             return "yellow";
+        }
+
+        if (query.toLowerCase().contains("bond")) {
+            return "Sean Connery";
         }
         return "as;lfkjas;lk";
 
